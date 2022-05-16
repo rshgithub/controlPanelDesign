06ff322b1d160483dc2e2796451035dd43bc3ac1
rshgithub <rawnsh2218@gmail.com>
2022-05-16 23:02:08 +0300

    Added REST Api

---
+++ resources/views/dashboard_cards/completedTasks.blade.php
@@ -0,0 +1,23 @@
+
+
+
+<div class="col-lg-4 mt-4 mb-3">
+    <div class="card z-index-2 ">
+        <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2 bg-transparent">
+            <div class="bg-gradient-dark shadow-dark border-radius-lg py-3 pe-1">
+                <div class="chart">
+                    <canvas id="chart-line-tasks" class="chart-canvas" height="170"></canvas>
+                </div>
+            </div>
+        </div>
+        <div class="card-body">
+            <h6 class="mb-0 ">Completed Tasks</h6>
+            <p class="text-sm ">Last Campaign Performance</p>
+            <hr class="dark horizontal">
+            <div class="d-flex ">
+                <i class="material-icons text-sm my-auto me-1">schedule</i>
+                <p class="mb-0 text-sm">just updated</p>
+            </div>
+        </div>
+    </div>
+</div>
